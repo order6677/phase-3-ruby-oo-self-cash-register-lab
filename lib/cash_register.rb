243a7780214ba1e1f63496a268cc99012803c17e
awadh <awadh.hassan@student.moringaschool.com>
2022-11-13 20:30:16 +0300

    resubmission

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,33 +1,36 @@
 class CashRegister
+   attr_accessor :total, :receipt
+   attr_reader :discount
+   attr_writer
 
-    attr_accessor :total,:receipt
-    attr_reader :discount
-    attr_writer
+   def initialize discount=0
+       @total = 0
+       @discount = discount
+       @receipt = []
+   end
 
-    def initialize discount = 0
-        @total = 0
-        @discount = discount
-        @receipt = []
-     end
-    end
-    def add_item title, price, qty=1
-        @total += price * qty
-        qty.times { @receipt.push ({title:title,price:price,qty:qty})}
-    end
+   def add_item title, price, qty=1
+       @total += price * qty
+       qty.times { @receipt.push ({title: title, price: price, qty: qty}) }
+   end
 
-    def apply_dicount
-           if @discount > 0
-            savings = @total * @discount/100
-            @total -= savings
-    end
-  def items
-     @receipt.map{ |item| item[:title] }
-  end
+   def apply_discount
+       if @discount > 0
+           savings = @total * @discount/100
+           @total -= savings
+           "After the discount, the total comes to $#{@total}."
+       else
+           "There is no discount to apply."
+       end
+   end
 
- def void_last_transaction
-    voided_item = receipt.pop
-    @total -= voided_item[:price] * voided_item[:qty]
- end
+   def items
+       @receipt.map{ |item| item[:title] }
+   end
+
+   def void_last_transaction
+       voided_item = receipt.pop
+       @total -= voided_item[:price] * voided_item[:qty]
+   end
+
 end
-
-
